6c055a2c1b9aad4fb0916886bc424ac4c64af12f
Anna Kolisz <annakolisz@10g.pl>
2022-10-20 15:55:38 +0200

    add form to ratings

--- src/app/components/review-form/review-form.component.ts
+++ src/app/components/review-form/review-form.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-review-form',
   templateUrl: './review-form.component.html',
   styleUrls: ['./review-form.component.scss']
 })
 export class ReviewFormComponent implements OnInit {
+
+  courses = [ "Intermadiate HTML & CSS", "Javascript - hard parts", "Angular from basic", "Vue - from zero to hero"]
 
   constructor() { }
 

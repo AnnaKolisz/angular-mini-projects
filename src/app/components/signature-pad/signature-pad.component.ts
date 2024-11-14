a4f3f2e6b55534454290211543e58249eb8cdd02
Anna Kolisz <annakolisz@10g.pl>
2024-11-14 17:28:12 +0100

    updates for signature

--- src/app/components/signature-pad/signature-pad.component.ts
+++ src/app/components/signature-pad/signature-pad.component.ts
@@ -6,25 +6,42 @@ import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
   styleUrls: ['./signature-pad.component.scss']
 })
 export class SignaturePadComponent {
-  
-  @ViewChild('signPad', {static: false}) signPad!: ElementRef<HTMLCanvasElement>;
-   signatureImg?: string = '';
-  private sigPadElement: any;
-  private context: any;
+
+  @ViewChild('canvas', { static: false }) canvasEL!: ElementRef<HTMLCanvasElement>;
+
+  @ViewChild('ctn', { static: false }) container!: ElementRef<HTMLDivElement>;
+  signatureImg?: string = '';
+  private canvas: any;
+  private context: CanvasRenderingContext2D;
   private isDrawing!: boolean;
 
-
-  public ngAfterViewInit(): void {
-    this.sigPadElement = this.signPad.nativeElement;
-    this.context = this.sigPadElement.getContext('2d');
+  // tutaj spróbuj zrobić dynamiczne z viewport
+  ngAfterViewInit(): void {
+    this.canvas = this.canvasEL.nativeElement;
+    const ctn = this.container.nativeElement;
+    this.canvas.width = (ctn.clientWidth / 2) - 64;
+    this.canvas.height = ctn.clientHeight - 16;
+    this.context = this.canvas.getContext('2d');
+    console.log(this.context)
     this.context.strokeStyle = '#004D40';
     this.context.lineWidth = 1;
-  }
 
-  onMouseDown(e: any): void {
+  }
+  /* what copilot said
+    function resizeCanvas() {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      // Additional logic to redraw graphics based on new size
+  }
+  
+  window.addEventListener('resize', resizeCanvas);
+  resizeCanvas();
+  */
+
+  onMouseDown(event: any): void {
     // The mouse button is clicked, which means the start of drawing the signature
     this.isDrawing = true;
-    const coords = this.relativeCoords(e);
+    const coords = this.relativeCoords(event);
     this.context.moveTo(coords.x, coords.y);
   }
 
@@ -38,22 +55,22 @@ export class SignaturePadComponent {
     if (this.isDrawing) { // if we're not drawing we need to ignore the events
       const coords = this.relativeCoords(e);
       this.context.lineTo(coords.x, coords.y);
-     //this.context.lineTo(e.clientX, e.clientY)
       this.context.stroke();
     }
   }
 
   clearSignature(): void {
     this.signatureImg = undefined;
-    this.context.clearRect(0, 0, this.sigPadElement.width, this.sigPadElement.height);
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.context.beginPath();
   }
 
   saveSignature(): void {
-    this.signatureImg = this.sigPadElement.toDataURL('image/png');
+    this.signatureImg = this.canvas.toDataURL('image/png');
   }
 
-  private relativeCoords(event: any): { x: number, y: number } {
+  //mouseevent
+  private relativeCoords(event: any) {
     const bounds = event.target.getBoundingClientRect();
     const cords = {
       clientX: event.clientX || event.changedTouches[0].clientX,
@@ -61,7 +78,7 @@ export class SignaturePadComponent {
     };
     const x = cords.clientX - bounds.left;
     const y = cords.clientY - bounds.top;
+    return { x, y };
 
-    return {x, y};
   }
 }

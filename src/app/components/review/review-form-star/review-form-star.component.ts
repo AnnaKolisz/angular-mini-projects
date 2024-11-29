e5a80ad61f14eb7faa5a979b61464ca7c253beca
Anna Kolisz <annakolisz@10g.pl>
2024-11-29 16:14:01 +0100

    add ControlValueAccessor to form-star

--- src/app/components/review/review-form-star/review-form-star.component.ts
+++ src/app/components/review/review-form-star/review-form-star.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, OnInit, Optional, Output, Self } from '@angular/core';
+import { Component, EventEmitter, inject, Input, OnDestroy, OnInit, Optional, Output, Self, signal } from '@angular/core';
 import { ControlValueAccessor, FormControl, NgControl, ReactiveFormsModule } from '@angular/forms';
 import { MatFormField, MatFormFieldControl, MatFormFieldModule } from '@angular/material/form-field';
 import { ReviewStarInputComponent } from './review-star-input/review-star-input.component';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -12,9 +13,9 @@ import { ReviewStarInputComponent } from './review-star-input/review-star-input.
   templateUrl: './review-form-star.component.html',
   styleUrl: './review-form-star.component.scss'
 })
-export class ReviewFormStarComponent implements OnInit {
+export class ReviewFormStarComponent implements OnInit, OnDestroy, ControlValueAccessor {
 
-  foo = new Map([
+  pointsToName = new Map([
     [1, "Terrible"],
     [2, "Bad"],
     [3, "OK"],
@@ -23,14 +24,42 @@ export class ReviewFormStarComponent implements OnInit {
   ]);
 
   rate = new FormControl<number>(null);
-  rateName: string;
+  rateName = signal('')
+  sub$: Subscription;
+  onTouched: () => {};
+  ngControl = inject(NgControl, { optional: true, self: true });
+
+  constructor() {
+    if (this.ngControl != null) {
+      this.ngControl.valueAccessor = this;
+    }
+  }
 
   ngOnInit(): void {
-    this.rate.valueChanges.subscribe(value => {
-      this.rateName = this.foo.get(value);
+    this.sub$ = this.rate.valueChanges.subscribe(val => {
+      this.rateName.set(this.pointsToName.get(val) || '');
     })
   }
 
+  ngOnDestroy(): void {
+    if (this.sub$) {
+      this.sub$.unsubscribe();
+    }
+  }
+
+
+  writeValue(obj: any): void {
+    this.rate.setValue(obj, { emitEvent: false });
+  }
+  registerOnChange(fn: any): void {
+    this.rate.valueChanges.subscribe(fn);
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    isDisabled ? this.rate.disable() : this.rate.enable();
+  }
 
 
 }
